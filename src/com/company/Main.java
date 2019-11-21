e0d8f4b94b1e02e0e11cb916098b508ee6a9d06a
Recruit <you@example.com>
2019-11-21 10:14:30 +0200

    Initial 'person' exercise commit with Person and Main java class

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person = new Person(
+                "Ryan",30,"male",
+                new String[] {"being a hardarse", "agile", "ssd hard drives"});
+        String greeting = person.hello();
+        System.out.println(greeting);
+        //person.hello();
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Person {
+
+    private String name;
+    private  int age;
+    private String gender;
+    private String[] interests;
+
+    public Person(String name, int age, String gender, String[] interests) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.interests=interests;
+    }
+
+
+    public String hello(){
+    String results = "";
+    for (int i=0; i<interests.length;i++){
+        results += interests[i];
+        if (i <interests.length-2){
+            results += ", ";
+        }
+        else if (i== interests.length-2){
+            results += " and ";
+        }
+    }
+        //return resuls;
+        System.out.print("Hello, my name is " + name + " I am " + age + " years old. " + " My interests are ");
+
+        //return null;
+
+
+        return results;
+    }
+}
