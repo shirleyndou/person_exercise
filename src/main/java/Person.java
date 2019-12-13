408b5af64a29d0e112991bd249dfd1a0714f9dee
Recruit <you@example.com>
2019-12-13 09:46:54 +0200

    Updated person exercise

---
+++ src/main/java/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+
+  private String name;
+  private int age;
+  private String gender;
+  private String[] interests;
+
+    public Person(String name, int age, String gender, String[] interests) {
+        this.name = name;
+        this.age=age;
+        this.gender=gender;
+        this.interests=interests;
+    }
+
+    public String hello()
+    {
+        String results ="";
+        for(int i=0; i<interests.length; i++)
+        {
+            results += interests[i];
+
+            if(i<interests.length-2)
+            {
+                results += ", ";
+            } else if(i==interests.length-2)
+            {
+                results +=" and ";
+            }
+        }
+        System.out.print("Hello, my name is " + name + " I am " + age + " years old. " + " My interests are ");
+
+
+        return results;
+
+    }
+}
